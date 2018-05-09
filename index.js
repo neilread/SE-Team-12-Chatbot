4654270d3f2f0910966b50fb658632480d46cf8b
Neil Read <neil.read14@outlook.com>
2018-05-09 18:06:50 +1200

    Minor Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const scr = require('./test');
 
 exports.helloHttp = function helloHttp (request, response) {
-  response.json({ fulfillmentText: scr.Test.t(request.fulfillmentText.) });
+  response.json({ fulfillmentText: scr.Test.t("Hello World!") });
 };
