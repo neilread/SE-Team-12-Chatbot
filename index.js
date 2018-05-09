666d2ed2ef2be9a511179bc42ea57724d741f153
Neil Read <neil.read14@outlook.com>
2018-05-09 21:11:07 +1200

    Added express module

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const scr = require('./test');
+const DialogflowApp = require('actions-on-google').DialogflowApp
+const express = require('express');
+let app = express();
+
+app.post('/', function (request, response) {
+  const  assistant = new DialogflowApp({request: request, response: response});
+  //request.body
+})
 /*const express = require('express');
 const bodyParser = require('body-parser');
 
