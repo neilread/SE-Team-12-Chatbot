b5df75da9ee71550af1673b623f9d632a8df3665
neilread <31087483+neilread@users.noreply.github.com>
2018-05-12 16:24:51 +1200

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
+//const functions = require('firebase-functions');
 const scr = require('./test');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+
+const server = express();
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+server.use(bodyParser.json());
+
+server.post('/process-intent', (req, res) => {
+  console.log("Hey, it worked!");
+  return res.json({
+    speech: 'Something went right!',
+    displayText: 'Something went right!',
+    source: 'get-movie-details'
+  });
+});
+
+server.listen((process.env.PORT || 8000), () => {
+  console.log("Server is up and running...");
+});
+//const API_KEY = require('./apiKey');
+
+
 /*const express = require('express');
 const bodyParser = require('body-parser');
 
 const restService = express();
 
-/*restService.use(bodyParser.urlEncoded({
+restService.use(bodyParser.urlEncoded({
   extended: true
 }));
 
@@ -14,8 +42,9 @@ restService.post('/echo', function(req, res) {
   
 });*/
 
-exports.webhook = (req, rsp) =>
+/*exports.webhook = (req, rsp) =>
 {
-  rsp.json({ fulfillmentText: req.queryId });
+  let s = JSON.stringify(req);
+  rsp.json({ fulfillmentText: "Hello" });
   //rsp.json({ fulfillmentText: "hi" });
-};
+};*/
