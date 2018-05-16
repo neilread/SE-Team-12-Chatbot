be0f6421aac67026f756ed23c8d466609330e15f
neilread <31087483+neilread@users.noreply.github.com>
2018-05-17 11:00:21 +1200

    Add files via upload

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -23,8 +23,8 @@ describe("findDegree()", () =>
             expect(link).toEqual("https://www.aut.ac.nz/study/study-options/engineering-computer-and-mathematical-sciences/courses/bachelor-of-computer-and-information-sciences");
             done();
         });
-    }, 30000);
-});
+    });
+}, 30000);
 
 describe("findMajor()", () =>
 {
@@ -58,7 +58,7 @@ describe("getPapersForMajor()", () =>
             done();
         }, "Bachelor of Computer and Information Sciences",
         "Engineering, computer and mathematical sciences");
-    }, 10000);
+    }, 30000);
 
     it("It should return an appropriate list of papers that will qualify a student for a specific major if the major's degree is not specified", (done) =>
     {
@@ -75,6 +75,29 @@ describe("getPapersForMajor()", () =>
             expect(codes).toEqual(["COMM501", "COMP500", "COMP501", "COMP502", "COMP503", "ENEL504", "INFS500", "MATH500", "MATH501", "MATH502", "STAT500",
             "COMP600", "COMP602", "COMP603", "INFS600", "INFS601", "COMP604", "INFS602", "COMP704", "COMP719", "ENSE701", "COMP713", "COMP721", "COMP505",
             "ENSE501", "ENSE502", "COMP612", "MATH604", "COMP705", "COMP710", "COMP716", "COMP720", "COMP724", "INFS700"]);
+            done();
+        });
+    }, 30000);
+});
+
+describe("getPapersForDegree()", () =>
+{
+    it("should return all papers for the first listed major of the degree", (done) =>
+    {
+        scrape.getPapersForDegree("Bachelor of Computer and Information Sciences", null, (papers) =>
+        {
+            let codes = [];
+            for(let i = 0; i < papers.length; i++)
+            {
+                codes[i] = papers[i].code;
+            }
+
+            console.log(codes);
+
+            expect(codes).toEqual(["COMM501, COMP500", "COMP501", "COMP502", "COMP503", "ENEL504", "INFS500", "MATH500", "MATH501", "MATH502", "STAT500", 
+            "COMP600", "INFS600", "STAT600", "STAT601", "INFS601", "STAT603", "COMP704", "STAT700", "STAT702", "COMP723", "STAT701", "COMP505",
+            "ENSE501", "ENSE502", "COMP612", "MATH604", "COMP705", "COMP710", "COMP716", "COMP720", "COMP724", "INFS700"]);
+
             done();
         });
     }, 30000);
